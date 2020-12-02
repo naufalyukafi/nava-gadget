1e6e5d06f0b0ac7e9fb0ed4a8e94c1caad848362
Naufal Yukafi <naufalyukafi10@gmail.com>
2020-12-03 06:23:37 +0700

    feat(ProjectDetailPage): implement ui project for detail page

---
+++ src/modules/LoggedIn/Projects/ProjectDetailPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./ProjectPage.css";
+import DetailCardPage from "./DetailCardPage";
+import { Layout, Typography, Row, Col, Input, Select, Button } from 'antd';
+const { Search } = Input;
+const { Option } = Select;
+
+const ProjectDetailPage = () => {
+    const onSearch = () => console.log("coba");
+    return (
+        <Layout>
+            <Layout.Header style={{ backgroundColor: '#fff' }}>
+                <Row align="middle" justify="space-around" >
+                    <Col>
+                        <Typography.Title level={4} style={{ fontWeight: 'bold' }}>Gadged Store</Typography.Title>
+                    </Col>
+                    <Col className="navbar-select">
+                        <Select
+                            showSearch
+                            style={{ width: 200 }}
+                            placeholder="Select a Language"
+                            optionFilterProp="children"
+                        >
+                            <Option value="indonesia">Indonesia</Option>
+                            <Option value="inggris">Inggris</Option>
+                        </Select>
+                    </Col>
+                    <Col className='navbar-search' >
+                        <Search placeholder="input search text" onSearch={onSearch} enterButton style={{ marginTop: 15, minWidth: 300 }} />
+                    </Col>
+                    <Col>
+                        <Button className="signup" type="primary" style={{ marginRight: 20 }}>DAFTAR</Button>
+                        <Button className="signin" type="primary">LOGIN</Button>
+                    </Col>
+                </Row>
+            </Layout.Header>
+            <Layout.Content className="site-content" style={{ padding: 50 }}>
+                {/* <div> */}
+                <DetailCardPage />
+                {/* </div> */}
+            </Layout.Content>
+            <Layout.Footer style={{ textAlign: 'center', fontWeight: 'bold', backgroundColor: '#fff' }}>
+                Create By Yukafi & Shifa
+        </Layout.Footer>
+        </Layout>
+    )
+}
+
+export default ProjectDetailPage
