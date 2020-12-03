d867cca6df0c8b9d0a8d0296c61205d083a6b200
Naufal Yukafi <naufalyukafi10@gmail.com>
2020-12-03 07:16:53 +0700

    feact(card): implement routing

--- src/Components/CardList.tsx
+++ src/Components/CardList.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Row, Col, Button, Typography, Card } from "antd"
 import { IProject } from "../modules/LoggedIn/Projects/ProjectListDumy"
+import { Link } from 'react-router-dom'
 const CardList = (props: IProject) => {
     return (
-        <Card hoverable
-            style={{ width: 300, borderRadius: 10 }}
-            cover={<img src={props.image} />} >
-            <Row align="middle" justify="space-between" style={{ fontWeight: "bold" }}>
-                <Col>{props.price}</Col>
-                <Button type="primary" style={{ fontWeight: "bold" }} >{props.category}</Button>
-            </Row>
-            <Row style={{ marginTop: 10 }}>
-                {props.description}
-            </Row>
-            <Typography.Text type="secondary">
-                {props.location}
-            </Typography.Text>
-        </Card>
+        <Link to='/project/detail'>
+            <Card hoverable
+                style={{ width: 300, borderRadius: 10 }}
+                cover={<img src={props.image} />} >
+                <Row align="middle" justify="space-between" style={{ fontWeight: "bold" }}>
+                    <Col>{props.price}</Col>
+                    <Button type="primary" style={{ fontWeight: "bold" }} >{props.category}</Button>
+                </Row>
+                <Row style={{ marginTop: 10 }}>
+                    {props.description}
+                </Row>
+                <Typography.Text type="secondary">
+                    {props.location}
+                </Typography.Text>
+            </Card>
+        </Link>
     )
 }
 
